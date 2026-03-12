fe71b940191925d7d6a7599b670f20e9650ab2b1
bddjr <bddjr08@outlook.com>
2026-03-12 17:16:54 +0800

    check Content-Type

--- src/CCWData-Code-Injection-Fix.user.js
+++ src/CCWData-Code-Injection-Fix.user.js
@@ -24,8 +24,30 @@ Element.prototype.appendChild = function (c) {
             console.log('CCWData-Code-Injection-Fix fetch ' + src)
             c.removeAttribute("src")
             fetch(src, { cache: 'default' })
-                .then(r => r.text())
-                .then(text => {
+                .then(r => {
+                    const type = r.headers.get('Content-Type')?.split(';')[0].trim()
+                    switch (type) {
+                        // https://htmlspecs.com/mimesniff/#javascript-mime-type
+                        case "application/ecmascript":
+                        case "application/javascript":
+                        case "application/x-ecmascript":
+                        case "application/x-javascript":
+                        case "text/ecmascript":
+                        case "text/javascript":
+                        case "text/javascript1.0":
+                        case "text/javascript1.1":
+                        case "text/javascript1.2":
+                        case "text/javascript1.3":
+                        case "text/javascript1.4":
+                        case "text/javascript1.5":
+                        case "text/jscript":
+                        case "text/livescript":
+                        case "text/x-ecmascript":
+                        case "text/x-javascript":
+                            return r.text()
+                    }
+                    throw `Refused to execute script from '${src}' because its MIME type ('${type}') is not executable.`
+                }).then(text => {
                     c.innerHTML = text.replace(/(?=\([^()]+\)\.push\()/, injectPrefix)
                     console.log('CCWData-Code-Injection-Fix', appendChild.call(this, c))
                 })
